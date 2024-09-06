92aa3b34cea3050b314a77b8c64b4aeaa8502e1d
solidity12 <volibear1001@gmail.com>
2024-09-06 19:47:10 +0900

    feat: add showChallengePage

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <template>
-  <LandingView />
+  <LandingView v-if="!showChallengePage" @clickBtn="clickPlayBtn" />
+  <div v-if="showChallengePage">{{ showChallengePage }}</div>
 </template>
 
 <script setup>
 import LandingView from "@/views/LandingView.vue";
+
+import { ref } from "vue";
+
+// Reactive state
+const showChallengePage = ref(false);
+
+// Methods
+const clickPlayBtn = async () => {
+  showChallengePage.value = true;
+};
 </script>
 
 <style scoped></style>
--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -1,9 +1,17 @@
 <template>
-  <LandingPage />
+  <LandingPage @clickBtn="clickBtn" />
 </template>
 
 <script setup>
 import LandingPage from "../components/landing/LandingPage.vue";
+
+// Define emits
+const emit = defineEmits(["clickBtn"]);
+
+// Methods
+const clickBtn = async () => {
+  emit("clickBtn");
+};
 </script>
 
 <style></style>
