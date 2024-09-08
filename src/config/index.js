75d04e448388fe63681c79fb771ef9803d933c67
lukepark327 <lukepark327@gmail.com>
2024-09-08 19:30:45 +0900

    feat: get NFTs

---
+++ src/config/index.js
@@ -0,0 +1,8 @@
+import contracts from "./contracts.json";
+
+const config = {
+    contracts,
+    rpc: "https://api.test.wemix.com" // TODO
+};
+
+export default config;
---
+++ src/utils/gameView.js
@@ -0,0 +1,71 @@
+import { ethers } from "ethers";
+
+import config from "@/config";
+
+import GameAbis from "@/abis/Game.json";
+import UsdcAbis from "@/abis/USDC.json";
+import BadgeAbis from "@/abis/Verified.json";
+
+let provider = undefined;
+// let signer = undefined;
+let contracts = {
+    Game: undefined,
+    Award: undefined,
+    Badge: undefined,
+};
+
+// TODO
+const GAS_INFO = {
+    gasPrice: "100000000001",
+}
+
+async function attach() {
+    if (!provider) {
+        provider = new ethers.JsonRpcProvider(config.rpc);
+    }
+
+    if (!contracts.Game) {
+        contracts.Game = new ethers.Contract(config.contracts.Game, GameAbis.abi, provider);
+    }
+    if (!contracts.Award) {
+        contracts.Award = new ethers.Contract(config.contracts.Award, UsdcAbis.abi, provider);
+    }
+    if (!contracts.Badge) {
+        contracts.Badge = new ethers.Contract(config.contracts.Badge, BadgeAbis.abi, provider);
+    }
+}
+
+function sleep(ms) {
+    return new Promise((r) => setTimeout(r, ms));
+}
+
+async function waitForTransaction(hash) {
+    const maxTime = 10; // TODO
+    for (let i = 0; i < maxTime; i++) {
+        const receipt = await provider.getTransactionReceipt(hash);
+        if (receipt) {
+            if (receipt.status == 1) {
+                return true
+            } else {
+                return false;
+            }
+        }
+        await sleep(1000);
+    }
+    return false;
+}
+
+/* View */
+
+async function getGames(
+    startNumber, endNumber
+) {
+    await attach();
+
+    let response = await contracts.Game.getNfts(startNumber, endNumber);
+    return response;
+}
+
+export {
+    getGames,
+}
