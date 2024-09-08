330f558f17c34b81cc796543e6aa79ba93234c79
solidity12 <volibear1001@gmail.com>
2024-09-08 14:50:15 +0900

    feat: add footer

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <TopBar v-if="route.path != '/'" />
   <router-view />
+  <Footer v-if="route.path != '/'" />
 </template>
 
 <script setup>
 import TopBar from "@/components/common/TopBar.vue";
+import Footer from "@/components/common/Footer.vue";
 
 import { useRoute, useRouter } from "vue-router";
 
---
+++ src/components/common/Footer.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="footer">
+    <div class="footer-content-wrapper">
+      <div class="community-wrapper">
+        <img src="@/assets/community/github.svg" alt="github">
+      </div>
+      <div class="copyright">ⓒ CTP. All Rights Reserved.</div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.footer {
+  display: flex;
+  margin-top: 70px;
+  padding: 40px 170px 50px 170px;
+  justify-content: space-between;
+  align-items: flex-end;
+
+  border-top: 2px solid #000;
+  background: #fff;
+}
+
+.footer-content-wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-end;
+  gap: 20px;
+  flex: 1 0 0;
+}
+
+.community-wrapper {
+  display: flex;
+  align-items: flex-start;
+  gap: 12px;
+}
+
+.copyright {
+  color: #000;
+  font-family: Archivo;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 19px;
+  letter-spacing: -0.4px;
+}
+</style>
