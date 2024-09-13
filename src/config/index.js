327339e0c526dcf32883d8e45dfabc567e0c976b
solidity12 <volibear1001@gmail.com>
2024-09-13 12:06:49 +0900

    feat: add base address

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,3 @@
-import contracts from "./wemixtestnet/contracts.json";
-
 const projectId = "d0ec522cc8ac196893d24355f948e04f"; //TODO env
 
 const projectName = "CTP";
@@ -7,13 +5,25 @@ const projectDescription = "Capture the Prompt";
 const projectUrl = "https://promptower.github.io/webapp/";
 const logoUrl = "https://raw.githubusercontent.com/promptower/webapp/main/src/assets/logo.svg";
 
-//TODO
-const chainId = 1112;
-const chainName = "Wemix Testnet";
-const currency = "WEMIX";
-const explorerUrl = "https://testnet.wemixscan.com/";
-const rpcUrl = "https://api.test.wemix.com";
-const chainImageUrl = ""; 
+// WEMIX
+// import contracts from "./wemixtestnet/contracts.json";
+
+// const chainId = 1112;
+// const chainName = "Wemix Testnet";
+// const currency = "WEMIX";
+// const explorerUrl = "https://testnet.wemixscan.com/";
+// const rpcUrl = "https://api.test.wemix.com";
+// const chainImageUrl = ""; 
+
+// BASE
+import contracts from "./basetestnet/contracts.json";
+
+const chainId = 84532;
+const chainName = "Base Sepolia Testnet";
+const currency = "ETH";
+const explorerUrl = "https://sepolia.basescan.org/";
+const rpcUrl = "https://sepolia.base.org";
+const chainImageUrl = "";
 
 const config = {
     projectId,
